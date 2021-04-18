e8a38f5359f32123fb57d08ce4aaf3062063443a
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2021-04-18 09:42:08 +0200

    setup.py → setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
-from setuptools import setup, find_packages
-
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import setup
 
 setup(
-    name='bsch',
-    version='1.0.dev0',
-    url = 'https://github.com/pklaus/bsch',
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'typing;python_version<"3.5"',
-    ],
-    extras_require={
-        "plotting":  ["numpy", "Pillow", "matplotlib"],
+    use_scm_version={
+        'version_scheme': 'post-release',
+        'local_scheme': 'dirty-tag'
     },
-    entry_points = {
-        'console_scripts': [
-            'gtc400c-plot = bsch.gtc400c.util:cli_plot',
-            'gtc400c-thermogram = bsch.gtc400c.util:cli_thermogram',
-            'gtc400c-blend = bsch.gtc400c.util:cli_blend',
-            'gtc400c-ftp = bsch.gtc400c.ftp:main',
-        ],
-    }
+    setup_requires=[
+        'setuptools_scm >= 1.7.0',
+        'setuptools >= 38.6.0'
+    ]
 )
